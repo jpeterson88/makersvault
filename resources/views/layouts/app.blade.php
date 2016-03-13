251a47268e8a1be9e00babd21f5354836fac444d
Justin Peterson <Sleepy@Justins-MacBook-Pro.local>
2016-03-13 15:33:31 -0700

    added basic styles and created home page

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Makers Vault</title>
+    <link rel="stylesheet" href="assets/css/app.css" type="text/css">
+    <script src="https://use.typekit.net/jsn2fsu.js"></script>
+
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,33 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('layouts.app')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="/assets/css/app.css" type="text/css">
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel Test</div>
-            </div>
-        </div>
-    </body>
-</html>
+@section('content')
+<section class="home">
+  <div class="row">
+    <div class="small-12 columns">
+      <h2 class="text-center">Product Name</h2>
+    </div>
+    <div class="small-12 large-8 columns">
+      <img src="http://placehold.it/800x600" alt="" />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Donec sit amet viverra arcu. Suspendisse blandit feugiat erat,
+        congue posuere magna dictum non. Nam euismod ac sem vitae vehicula.
+        Curabitur non ultrices orci, sed ultricies diam. Suspendisse potenti.
+        Duis sollicitudin mi posuere auctor varius. Proin lacinia neque sed
+        turpis feugiat, ac posuere orci aliquet.
+      </p>
+      <div>
+        <a href="#" class="button expanded">Purchase for $20</a>
+      </div>
+    </div>
+
+    <div class="class-12 large-4 columns">
+      <div class="callout">
+        <p>Maker: Sleepy</p>
+
+      </div>
+
+    </div>
+  </div>
+</section>
+@endsection
